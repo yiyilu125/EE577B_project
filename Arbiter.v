c8489d0b6174c5897bcf0b712add02f6d2db1c01
YuhangHeUSC <heyuhang@usc.edu>
2024-10-22 13:34:42 -0700

    Update Arbiter.v
    1:logic now is based on pro. tabar's instruction on lecture on 7
    2:clk is canceled

--- Arbiter.v
+++ Arbiter.v
@@ -1,5 +1,4 @@
 module round_robin_arbiter (
-    input wire clk,
     input wire rst_n,              
     input wire [3:0] req0,         
     input wire [3:0] req1,
@@ -8,62 +7,126 @@ module round_robin_arbiter (
     output reg [3:0] grant        
 );
 
-    reg [1:0] current_grant;      
-
-    always @(posedge clk ) begin
+  
+	parameter state_req0=4'b0001;
+	parameter state_req1=4'b0010;
+	parameter state_req2=4'b0100;
+	parameter state_req3=4'b1000;
+	
+	
+	reg [3:0]state;	
+	
+	
+	
+    always @(*) begin
         if (rst_n) begin
-            grant <= 4'b0000;
-            current_grant <= 2'b00;
-        end else 
+            grant = 4'b0000;
+            state = state_req0;
+        end 
+		else 
 		begin
-            case (current_grant)
-                2'b00: begin
-                    if (req0 == 4'b0001) 
-					begin
-                        grant <= req0;
-                        current_grant <= 2'b01;  
-                    end 
-					else 
-					begin
-                        current_grant <= 2'b01; 
-                    end
-                end
-                2'b01: 
+            case (state)
+                state_req0: 
 				begin
-                    if (req1 == 4'b0010) 
-					begin
-                        grant <= req1;
-                        current_grant <= 2'b10;
-                    end 
-					else 
-					begin
-                        current_grant <= 2'b10;
-                    end
+                   if(req0==4'b0001)
+				   begin
+						grant=req0;
+						state=state_req1;
+				   end
+				   else if(req0!=4'b0001&&req1==4'b0010)
+				   begin
+						grant=req1;
+						state=state_req2;
+				   end
+				   else if (req1!=4'b0010&&req2==4'b0100)
+				   begin
+						grant=req2;
+						state=state_req3;
+				   end
+				   else if (req2!=4'b0100&&req3==4'b1000)
+				   begin
+						grant=req3;
+						state=state_req0;
+				   end	
                 end
-                2'b10: 
+
+				
+                state_req1: 
+				begin				 
+				 if(req1==4'b0010)
+				   begin
+						grant=req1;
+						state=state_req2;
+				   end
+				   else if(req1!=4'b0010&&req2==4'b0100)
+				   begin
+						grant=req2;
+						state=state_req3;
+				   end
+				   else if (req2!=4'b0100&&req3==4'b1000)
+				   begin
+						grant=req3;
+						state=state_req0;
+				   end
+				   else if (req3!=4'b1000&&req0==4'b0001)
+				   begin
+						grant=req0;
+						state=state_req1;
+				   end	 
+                end
+				
+
+                state_req2: 
 				begin
-                    if (req2 == 4'b0100) 
-					begin
-                        grant <= req2;
-                        current_grant <= 2'b11;
-                    end 
-					else 
-					begin
-                        current_grant <= 2'b11;
-                    end
+				 if(req2==4'b0100)
+				   begin
+						grant=req2;
+						state=state_req3;
+				   end
+				   else if(req2!=4'b0100&&req3==4'b1000)
+				   begin
+						grant=req3;
+						state=state_req0;
+				   end
+				   else if (req3!=4'b1000&&req0==4'b0001)
+				   begin
+						grant=req0;
+						state=state_req1;
+				   end
+				   else if (req0!=4'b0001&&req1==4'b0010)
+				   begin
+						grant=req1;
+						state=state_req2;
+				   end	 
                 end
-                2'b11: 
+				
+				
+				
+                state_req3: 
 				begin
-                    if (req3 == 4'b1000) 
-					begin
-                        grant <= req3;
-                        current_grant <= 2'b00;
-                    end 
-					else 
-					begin
-                        current_grant <= 2'b00;
-                    end
-                end
+				 if(req3==4'b1000)
+				   begin
+						grant=req3;
+						state=state_req0;
+				   end
+				   else if(req3!=4'b0100&&req0==4'b0001)
+				   begin
+						grant=req0;
+						state=state_req1;
+				   end
+				   else if (req0!=4'b0001&&req1==4'b0010)
+				   begin
+						grant=req1;
+						state=state_req2;
+				   end
+				   else if (req1!=4'b0010&&req2==4'b0100)
+				   begin
+						grant=req2;
+						state=state_req3;
+				   end	 
+				end
+				
+				
             endcase
         end
     end
